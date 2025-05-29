e8cd0f126b763dca53b3ec14c299bfc1e38f8d8c
HaithamMahajna <haithammahajna98@gmail.com>
2025-05-29 13:42:13 +0300

    update

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,8 +4,7 @@
 set -e
 
 echo "Starting deployment..."
-
-cd ~/${REPO_NAME}
+git checkout dev
 
 if [ ! -d "venv" ] && [ ! -d ".venv" ]; then
     echo "Creating virtual environment..."
@@ -25,12 +24,19 @@ pkill -f "uvicorn" || true
 sudo systemctl daemon-reload
 sudo systemctl restart polybot.service
 sudo systemctl enable polybot.service
-sudo systemctl status polybot.service --no-pager
 
+sudo systemctl restart otelcol
+
+sudo systemctl status polybot.service --no-pager
 if ! systemctl is-active --quiet polybot.service; then
   echo "❌ polybot.service is not running."
   sudo systemctl status polybot.service --no-pager
   exit 1
 fi
 
+if ! systemctl is-active --quiet otelcol.service; then
+  echo "❌ yolo.service is not running."
+  sudo systemctl status otelcol.service --no-pager
+  exit 1
+fi
 
