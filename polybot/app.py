65e65fc0642b718f228ee2fff3a702c44facced9
HaithamMahajna <haithammahajna98@gmail.com>
2025-06-23 02:59:11 +0300

    SQS

--- polybot/app.py
+++ polybot/app.py
@@ -1,13 +1,15 @@
-import flask
+import flask , jsonify
 from flask import request
 import os
 from .bot import Bot, QuoteBot, ImageProcessingBot
+import boto3
 
 app = flask.Flask(__name__)
 
 TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 BOT_APP_URL = os.environ['BOT_APP_URL']
 
+bot = ImageProcessingBot(TELEGRAM_BOT_TOKEN, BOT_APP_URL)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -19,6 +21,60 @@ def webhook():
     req = request.get_json()
     bot.handle_message(req['message'])
     return 'Ok'
+
+
+@app.route("/predictions/<prediction_id>", methods=["POST"])
+def get_prediction(prediction_id):
+    data = request.get_json()
+    chat_id = data.get("chat_id")
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    prediction_table = dynamodb.Table('HaithamPredictionSessions')
+    detection_table = dynamodb.Table('HaithamDetectionSessions')
+    print (f"Returning chatID : {chat_id}")
+    try:
+        prediction = prediction_table.get_item(Key={'uid': prediction_id}).get('Item')
+        if not prediction:
+            return jsonify({"error" : "Prediction not found"}), 404
+                
+    # Get detection objects
+        request = detection_table.query(
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('prediction_uid').eq(prediction_id)
+        )
+        detection_objects = request.get('Items', [])
+
+        data ={
+            "uid": prediction_id,
+            "original_image": prediction.get("original_image"),
+            "predicted_image": prediction.get("predicted_image"),
+            "detection_objects": [
+                {
+                    "label": obj["label"],
+                    "score": float(obj["score"]),
+                    "box": obj["box"]
+                } for obj in detection_objects
+            ]
+        }
+    except Exception as e:
+        return jsonify({"error": f"SDynamoDB error: {str(e)}"}), 500
+        
+    if prediction_id:
+        if data:
+            data = data.json()
+            detection_objects = data.get("detection_objects", [])
+            labels = [obj["label"] for obj in detection_objects]
+            detection_msg = f"Detected objects:\n" + "\n".join(labels) if labels else "No objects detected."
+            bot.send_text(chat_id, detection_msg)
+            return jsonify({"status": "ok"})
+        else:
+            bot.send_text(chat_id, f"YOLO server error: {request.status_code}")
+            return jsonify({"error": f"Server error: {str(e)}"}), 500
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
--- polybot/bot.py
+++ polybot/bot.py
@@ -77,54 +77,6 @@ class Bot:
     def upload_to_s3(self, local_file_path, s3_key):
         self.s3_client.upload_file(local_file_path, self.s3_bucket_name, s3_key)
 
-    app = Flask(__name__)
-    @app.route("/predictions/<prediction_id>", methods=["POST"])
-    def get_prediction(self,prediction_id):
-        data = request.get_json()
-        chat_id = data.get("chat_id")
-        dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-        prediction_table = dynamodb.Table('HaithamPredictionSessions')
-        detection_table = dynamodb.Table('HaithamDetectionSessions')
-        print (f"Returning chatID : {chat_id}")
-        try:
-            prediction = prediction_table.get_item(Key={'uid': prediction_id}).get('Item')
-            if not prediction:
-                return jsonify({"error" : "Prediction not found"}), 404
-                
-        # Get detection objects
-            request = detection_table.query(
-                KeyConditionExpression=boto3.dynamodb.conditions.Key('prediction_uid').eq(prediction_id)
-            )
-            detection_objects = request.get('Items', [])
-
-            data ={
-                "uid": prediction_id,
-                "original_image": prediction.get("original_image"),
-                "predicted_image": prediction.get("predicted_image"),
-                "detection_objects": [
-                    {
-                        "label": obj["label"],
-                        "score": float(obj["score"]),
-                        "box": obj["box"]
-                    } for obj in detection_objects
-                ]
-            }
-        except Exception as e:
-            return jsonify({"error": f"SDynamoDB error: {str(e)}"}), 500
-        
-        if prediction_id:
-            if data:
-                data = data.json()
-                detection_objects = data.get("detection_objects", [])
-                labels = [obj["label"] for obj in detection_objects]
-                detection_msg = f"Detected objects:\n" + "\n".join(labels) if labels else "No objects detected."
-                self.send_text(chat_id, detection_msg)
-                return jsonify({"status": "ok"})
-            else:
-                self.send_text(chat_id, f"YOLO server error: {request.status_code}")
-                return jsonify({"error": f"Server error: {str(e)}"}), 500
-                
-
 
 
     class ImageNameRequest(BaseModel):
@@ -254,52 +206,3 @@ class ImageProcessingBot(Bot):
 
 
 
-
-
-app = Flask(__name__)
-bot = ImageProcessingBot()
-@app.route("/predictions/<prediction_id>", methods=["POST"])
-def get_prediction(prediction_id):
-    data = request.get_json()
-    chat_id = data.get("chat_id")
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    prediction_table = dynamodb.Table('HaithamPredictionSessions')
-    detection_table = dynamodb.Table('HaithamDetectionSessions')
-    print (f"Returning chatID : {chat_id}")
-    try:
-        prediction = prediction_table.get_item(Key={'uid': prediction_id}).get('Item')
-        if not prediction:
-            return jsonify({"error" : "Prediction not found"}), 404
-                
-    # Get detection objects
-        request = detection_table.query(
-            KeyConditionExpression=boto3.dynamodb.conditions.Key('prediction_uid').eq(prediction_id)
-        )
-        detection_objects = request.get('Items', [])
-
-        data ={
-            "uid": prediction_id,
-            "original_image": prediction.get("original_image"),
-            "predicted_image": prediction.get("predicted_image"),
-            "detection_objects": [
-                {
-                    "label": obj["label"],
-                    "score": float(obj["score"]),
-                    "box": obj["box"]
-                } for obj in detection_objects
-            ]
-        }
-    except Exception as e:
-        return jsonify({"error": f"SDynamoDB error: {str(e)}"}), 500
-        
-    if prediction_id:
-        if data:
-            data = data.json()
-            detection_objects = data.get("detection_objects", [])
-            labels = [obj["label"] for obj in detection_objects]
-            detection_msg = f"Detected objects:\n" + "\n".join(labels) if labels else "No objects detected."
-            bot.send_text(chat_id, detection_msg)
-            return jsonify({"status": "ok"})
-        else:
-            bot.send_text(chat_id, f"YOLO server error: {request.status_code}")
-            return jsonify({"error": f"Server error: {str(e)}"}), 500
